92e1798fe453a4aa31e9d0cf485a6e31363b402e
DelOlmo05 <delolmolaraa@gmail.com>
2025-04-10 14:00:07 +0200

    commit con error en Saludo.java

--- src/Saludo.java
+++ src/Saludo.java
@@ -1,5 +1,5 @@
 public class Saludo {
     public static void decirHola() {
-        System.out.println("¡Hola desde la clase Saludo!");
+        System.out.println("¡Hola desde la clase Saludo!" // ERROR: falta cerrar paréntesis y punto y coma
     }
 }
